6f1e7612de0f9f50ce363f75274c64ec8a56a0a3
junsup <Jun@Juns-MacBook-Air.local>
2015-11-15 01:03:32 -0800

    weird

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:junsup/personal_website.git',
           branch: 'gh-pages'
         }
       }
